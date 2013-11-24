8bb6082aeb5e6eb6ed92db1bf6eda1f05280d09e
marco <m.gablowsky@googlemail.com>
2013-11-24 18:17:20 +0100

    added first RegistryService test and required resources

---
+++ vpinmame/src/test/java/de/mag/hypercab/app/vpinmame/registry/RegistryServiceTest.java
@@ -0,0 +1,25 @@
+package de.mag.hypercab.app.vpinmame.registry;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import de.mag.hypercab.AbstractIntegrationTest;
+import de.mag.hypercab.api.ini.SectionVO;
+
+public class RegistryServiceTest extends AbstractIntegrationTest {
+
+	@Resource
+	private RegistryService registryService;
+
+	@Test
+	public void registryFileIsLoaded() {
+		List<SectionVO> romSettings = registryService.getRomSettings();
+		Assert.assertNotNull(romSettings);
+		Assert.assertEquals(romSettings.size(), 53);
+	}
+
+}
