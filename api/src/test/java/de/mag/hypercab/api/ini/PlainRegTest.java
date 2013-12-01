aa9899d711fc171c71b10c1adb3912207fe4a02d
marco <m.gablowsky@googlemail.com>
2013-12-01 19:18:14 +0100

    added test to verify problem in ini4j library - written registry file cannot be loaded again because empty values are not stored as ""

---
+++ api/src/test/java/de/mag/hypercab/api/ini/PlainRegTest.java
@@ -0,0 +1,21 @@
+package de.mag.hypercab.api.ini;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.ini4j.Reg;
+import org.testng.annotations.Test;
+
+public class PlainRegTest {
+
+	@Test
+	public void readWriteRegFile() throws IOException {
+
+		File testFile = new File("target/test-classes/vpinmame.reg");
+		Reg reg = new Reg();
+		reg.getConfig().setEmptyOption(true);
+		reg.load(testFile);
+		reg.store(testFile);
+	}
+
+}
